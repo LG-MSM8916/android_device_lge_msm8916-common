95a4d9ca3d652a6867de185fe07369da93c6645b
carlosarriagacm <carlosarriagacm@gmail.com>
2017-09-19 08:57:58 -0500

    msm8916-common: removed packages for now

--- msm8916.mk
+++ msm8916.mk
@@ -85,17 +85,17 @@ PRODUCT_PACKAGES += \
     e2fsck
 
 # Gestures and Glove mode package
-PRODUCT_PACKAGES += \
-    DeviceSettings
+#PRODUCT_PACKAGES += \
+#    DeviceSettings
 
 # Jelly
-PRODUCT_PACKAGES += \
-    Jelly
+#PRODUCT_PACKAGES += \
+#    Jelly
 
 # FM packages
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
+#PRODUCT_PACKAGES += \
+#    FMRadio \
+#    libfmjni
 
 # IRSC
 PRODUCT_COPY_FILES += \
