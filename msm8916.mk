13fc37ea2094f7efd1884c832e8e2a7485e6478e
carlosarriagacm <carlosarriagacm@gmail.com>
2017-09-22 11:01:33 -0500

    msm8916-common: update audio for O

--- msm8916.mk
+++ msm8916.mk
@@ -43,21 +43,21 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    audiod \
-    audio.a2dp.default \
     audio.primary.msm8916 \
-    audio.r_submix.default \
+    audio.a2dp.default \
     audio.usb.default \
-    audio_policy.msm8916 \
-    tinymix
+    audio.r_submix.default
 
 PRODUCT_PACKAGES += \
-    libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
