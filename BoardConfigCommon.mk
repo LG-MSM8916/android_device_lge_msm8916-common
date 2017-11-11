83d6bd3f4a836fc5226a4e97ca958096ba901327
dom133 <dom133_pl@protonmail.com>
2017-11-11 21:49:36 +0100

    msm8916-common: add BOARD_KERNEL_IMAGE_NAME

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 an
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0008000 --ramdisk_offset 0x2000000
 
