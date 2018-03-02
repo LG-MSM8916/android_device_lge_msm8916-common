e2041f3c46d15c54ac8c4865140859da0567114f
Vineeth Penugonda <vineeth.pen@outlook.com>
2018-03-03 00:23:24 +0530

    Moved libwvm shim to common tree
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

--- msm8916.mk
+++ msm8916.mk
@@ -136,6 +136,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
+# Widevine
+PRODUCT_PACKAGES += \
+   libwvm_shim
+
 # OMX
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
