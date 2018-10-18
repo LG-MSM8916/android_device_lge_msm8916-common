b02b7add9cd3d6e9e03c8324c6f235ad61083ee8
PVineeth <vineeth.pen@gmail.com>
2018-10-18 00:04:21 -0500

    c90: Removed spaces
    Signed-off-by: PVineeth <vineeth.pen@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -200,8 +200,8 @@ PRODUCT_PACKAGES += \
 # GNSS HAL
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0 \
-	android.hardware.gnss@1.0-impl \
-	android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
