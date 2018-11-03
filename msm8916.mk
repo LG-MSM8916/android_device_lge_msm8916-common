5a788214ecac84c4ae73cd22e976537cf2d02055
PVineeth <vineeth.pen@outlook.com>
2018-11-03 03:33:26 -0500

    c90: Fix Keymaster

--- msm8916.mk
+++ msm8916.mk
@@ -128,7 +128,6 @@ PRODUCT_COPY_FILES += \
 # Keystore
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
     keystore.msm8916
 
 # Media
