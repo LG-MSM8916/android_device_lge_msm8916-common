86e629d04169dd37b06e409e9a673458d6c21bff
Volham22 <corentin.machu@orange.fr>
2018-11-04 15:13:49 +0100

    Trying to fix HAL
    Change-Id: Ie83bef75c136ec9bef416b633a75b4fff393b7d6

--- msm8916.mk
+++ msm8916.mk
@@ -159,7 +159,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.0-service \
-    power.msm8916
+    power.qcom
 
 # QMI
 PRODUCT_PACKAGES += \
